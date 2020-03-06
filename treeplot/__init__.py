6969c506dc466dbf91876d05d2cb9f7be7950696
erdogant <erdogant@gmail.com>
2020-03-06 22:17:12 +0100

    0.1.1

--- treeplot/__init__.py
+++ treeplot/__init__.py
@@ -7,7 +7,7 @@ from treeplot.treeplot import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # module level doc-string
 __doc__ = """
