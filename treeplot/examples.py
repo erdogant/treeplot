15d9595604c36aa9d59431f73ff8245fa63aa5bc
erdogant <erdogant@gmail.com>
2022-03-12 10:24:43 +0100

    update examples

--- treeplot/examples.py
+++ treeplot/examples.py
@@ -1,51 +1,102 @@
-import treeplot
-print(dir(treeplot))
-print(treeplot.__version__)
+import treeplot as tree
+print(dir(tree))
+print(tree.__version__)
 
 
 # %% Make example dataset
-from sklearn import tree
-X,y = treeplot.import_example('iris')
-
-
-# %% RandromForest EXAMPLE
 from sklearn.tree import DecisionTreeClassifier
-model_dt = DecisionTreeClassifier(max_depth=2, random_state=0).fit(X, y)
 
-ax = treeplot.randomforest(model_dt)
-tree.plot_tree(model_dt) 
+from sklearn import datasets
+data = datasets.load_iris()
+data = datasets.load_breast_cancer()
+
+# Import treeplot library
+import treeplot as tree
+
+# Learn model
+model = DecisionTreeClassifier(max_depth=4, random_state=0).fit(data.data, data.target)
+
+# Plot the tree
+ax = tree.plot(model, featnames=data.feature_names)
+
+# %% Make GradientBoostingClassifier example dataset
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import datasets
+data = datasets.load_breast_cancer()
+# data = datasets.load_iris()
+
+# Import treeplot library
+import treeplot as tree
+
+# Learn model
+model = GradientBoostingClassifier().fit(data.data, data.target)
+
+# Plot the tree
+ax = tree.plot(model, featnames=data.feature_names)
+
+# %% Make LightBM example dataset
+from lightgbm import LGBMClassifier
+from sklearn import datasets
+data = datasets.load_breast_cancer()
+# data = datasets.load_iris()
+
+# Import treeplot library
+import treeplot as tree
+
+# Learn model
+model = LGBMClassifier().fit(data.data, data.target)
+
+# Plot the tree
+ax = tree.plot(model, featnames=data.feature_names)
+
+
+# %% Make XGBOOST example dataset
+# Library to for model
+import xgboost as xgb
+from sklearn import datasets
+data = datasets.load_breast_cancer()
+# data = datasets.load_iris()
+
+# Import treeplot library
+import treeplot as tree
+
+# Learn model
+model = xgb.XGBClassifier(use_label_encoder=False, n_estimators=10, max_depth=5, random_state=0, eval_metric='logloss').fit(data.data, data.target)
+
+# Plot the tree
+ax = tree.plot(model, featnames=data.feature_names, num_trees=5)
 
 
 # %% RandromForest EXAMPLE
 from sklearn.ensemble import RandomForestClassifier
 model_rf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0).fit(X, y)
 
-ax = treeplot.randomforest(model_rf, export='png')
-ax = treeplot.randomforest(model_rf, export='pdf')
+ax = tree.randomforest(model_rf, export='png')
+ax = tree.randomforest(model_rf, export='pdf')
 
 # %% Gradientboosting example
-from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 gb = GradientBoostingClassifier()
 model_gradientboost = gb.fit(X, y)
 
-ax = treeplot.plot(model_gradientboost)
+ax = tree.plot(model_gradientboost)
 
 # %% XGBOOST EXAMPLE
 import xgboost as xgb
 model_xgb = xgb.XGBClassifier(n_estimators=100, max_depth=2, random_state=0).fit(X, y)
 
-ax = treeplot.plot(model_xgb)
-ax = treeplot.xgboost(model_xgb, plottype='vertical')
+ax = tree.plot(model_xgb)
+ax = tree.xgboost(model_xgb, plottype='vertical')
 
 # %% XGBOOST EXAMPLE
 from xgboost import XGBClassifier
 model_xgb = XGBClassifier(n_estimators=100, max_depth=2, random_state=0).fit(X, y)
 
-ax = treeplot.xgboost(model_xgb)
-ax = treeplot.xgboost(model_xgb, plottype='vertical')
+ax = tree.xgboost(model_xgb)
+ax = tree.xgboost(model_xgb, plottype='vertical')
 
 # %% Auto detect model
-ax = treeplot.plot(model_rf, verbose=4)
-ax = treeplot.plot(model_gradientboost, verbose=4)
-ax = treeplot.plot(model_dt, verbose=4)
-ax = treeplot.plot(model_xgb, verbose=4)
+ax = tree.plot(model_rf, verbose=4)
+ax = tree.plot(model_gradientboost, verbose=4)
+ax = tree.plot(model_dt, verbose=4)
+ax = tree.plot(model_xgb, verbose=4)
