5179263b7661c4aafbd29977669fff5a4ffbae94
erdogant <erdogant@gmail.com>
2024-09-30 14:08:09 +0200

    cleaning imports

--- treeplot/treeplot.py
+++ treeplot/treeplot.py
@@ -8,13 +8,12 @@
 # --------------------------------------------------
 
 # %% Libraries
-import os
-import sys
-import zipfile
+# import os
+# import sys
+# import zipfile
 import numpy as np
 from sklearn.tree import export_graphviz
-# from sklearn.tree.export import export_text
-from sklearn.tree import export_text
+# from sklearn.tree import export_text
 from subprocess import call
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
